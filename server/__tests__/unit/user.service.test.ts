2cbea7fbcf68085fbdbb55d89350fe95174f07bc
hxtruong <hxtruong6@gmail.com>
2022-08-19 17:19:15 +0900

    feat: write unit test for video service + modify email for user unit test

--- server/__tests__/unit/user.service.test.ts
+++ server/__tests__/unit/user.service.test.ts
@@ -1,14 +1,14 @@
 import userService from '../../services/user.service';
 
-const email = 'unit-test-user@gmail.com';
+const email = `unit_test_user_${new Date().getTime()}@gmail.com`;
 const password = 'unit-test-user-password';
 
-describe('[Integration test for Server]', () => {
+describe('unit test for Server', () => {
 	it('create user in Database', async () => {
 		expect.hasAssertions();
 		const response = await userService.create({ email, password });
 
-		console.log(response);
+		// console.log(response);
 		expect(response).toBeTruthy();
 	});
 });
